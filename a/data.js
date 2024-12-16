cb1ea1af0a6c153ab9122f2f1fc5dce161edbfb1
estebe2000 <steeve.pytel@gmail.com>
2024-12-16 21:18:48 +0100

    a

---
+++ a/data.js
@@ -0,0 +1,322 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-0_a",
+      "name": "0_A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.349364891507996,
+        "pitch": 0.1258650902880767,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.310666073649001,
+          "pitch": 0.18393574572725235,
+          "rotation": 0,
+          "target": "3-bat_c_dehors_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2_bat_b",
+      "name": "2_bat_b",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.9430611066395915,
+        "pitch": 0.22683502363161878,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.602534253706154,
+          "pitch": 0.40737502666831915,
+          "rotation": 0,
+          "target": "3-bat_c_dehors_"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.579020705133887,
+          "pitch": 0.281508888162044,
+          "title": "étage 1",
+          "text": "<br>"
+        },
+        {
+          "yaw": 2.5780257415765417,
+          "pitch": -0.24074923213774113,
+          "title": "étage 2",
+          "text": "<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "2-derrire-d",
+      "name": "derrière d",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.380163281650633,
+        "pitch": 0.07513129734270052,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9585353526721399,
+          "pitch": 0.03808734334663555,
+          "rotation": 0,
+          "target": "4-dehors_c"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1628941655348335,
+          "pitch": 0.05216202677666715,
+          "title": "Bâtiment D",
+          "text": "<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "3-bat_c_dehors_",
+      "name": "Bat_c_dehors_",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.6764706974045183,
+        "pitch": 0.04279413069793847,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3118673216609338,
+          "pitch": 0.03721010345726583,
+          "rotation": 0,
+          "target": "4-dehors_c"
+        },
+        {
+          "yaw": 2.090029316230014,
+          "pitch": 0.11619640176282608,
+          "rotation": 0,
+          "target": "5-mdl"
+        },
+        {
+          "yaw": 0.9868063303591885,
+          "pitch": 0.06128650266697022,
+          "rotation": 0,
+          "target": "0-0_a"
+        },
+        {
+          "yaw": -0.10220673417665793,
+          "pitch": -0.019128077885889994,
+          "rotation": 0,
+          "target": "1-2_bat_b"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.591172169370214,
+          "pitch": -0.046107585729213696,
+          "title": "Cantine",
+          "text": "Entrée de la cantine"
+        },
+        {
+          "yaw": -0.1648102220756158,
+          "pitch": -0.04569740911738407,
+          "title": "Bâtiment B",
+          "text": "entrée"
+        }
+      ]
+    },
+    {
+      "id": "4-dehors_c",
+      "name": "Dehors_c",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.7897402451751425,
+        "pitch": 0.21145335168393053,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9585518974394347,
+          "pitch": 0.003051833411625182,
+          "rotation": 6.283185307179586,
+          "target": "2-derrire-d"
+        },
+        {
+          "yaw": 1.0974683183419813,
+          "pitch": 0.042926050905021995,
+          "rotation": 0,
+          "target": "3-bat_c_dehors_"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.2144270369052723,
+          "pitch": -0.08038074443048515,
+          "title": "cantine",
+          "text": "<div><br></div>"
+        },
+        {
+          "yaw": -2.6222750726432267,
+          "pitch": -0.11326922405614859,
+          "title": "Bâtiment D",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "5-mdl",
+      "name": "mdl",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.0791813856036363,
+        "pitch": 0.1742235402079757,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7551049492296578,
+          "pitch": 0.02593532673282084,
+          "rotation": 0,
+          "target": "3-bat_c_dehors_"
+        },
+        {
+          "yaw": -1.9435398283222565,
+          "pitch": 0.05312946014742792,
+          "rotation": 0,
+          "target": "2-derrire-d"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7719848838554917,
+          "pitch": -0.005509137917794504,
+          "title": "MDL",
+          "text": "Porte de la MDL"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
