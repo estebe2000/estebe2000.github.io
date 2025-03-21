5539f798fbb8afa4b51543f7ba35b0dd1952175e
estebe2000 <steeve.pytel@gmail.com>
2025-03-21 09:19:38 +0100

    visite add

---
+++ visite/Theme5/Marzipano/data.js
@@ -0,0 +1,534 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-parking",
+      "name": "Parking",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.6000759469072516,
+        "pitch": 0.01484991211490616,
+        "fov": 1.525601287706261
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6942530465310845,
+          "pitch": 0.13982990134140927,
+          "rotation": 0,
+          "target": "1-bat_a"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.1232842070385303,
+          "pitch": 0.03827486891318799,
+          "title": "Abri deux roues",
+          "text": "Ici, un abri est mis à disposition afin de garer les deux roues."
+        }
+      ]
+    },
+    {
+      "id": "1-bat_a",
+      "name": "Bat_A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.1397937883165348,
+        "pitch": 0.24898556707482555,
+        "fov": 1.525601287706261
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.127817710435199,
+          "pitch": 0.05365749336783843,
+          "rotation": 0,
+          "target": "2-bat_e"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1421074258691224,
+          "pitch": 0.05051716142594742,
+          "title": "Machine à boissons",
+          "text": "Machine mise en place après une révolution digne de 1789 pour la modeste somme de 60 centimes, vous avez un café de qualité \"convenable\"."
+        },
+        {
+          "yaw": -0.42819565440267127,
+          "pitch": 0.11831210435722284,
+          "title": "Casiers",
+          "text": "Ces casiers sont attribués en début d'années à des groupes de 3 élèves pour permettre d'alléger les sacs."
+        },
+        {
+          "yaw": 0.5759844837689876,
+          "pitch": 0.04142613569834985,
+          "title": "Toilettes homme",
+          "text": "Tout est dans le titre"
+        },
+        {
+          "yaw": -1.3293922482714624,
+          "pitch": 0.06081346995287085,
+          "title": "Toilettes femme",
+          "text": "De même"
+        }
+      ]
+    },
+    {
+      "id": "2-bat_e",
+      "name": "Bat_E",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.4588631936284795,
+        "pitch": 0.14831512758388854,
+        "fov": 1.525601287706261
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.337349488663355,
+          "pitch": 0.6108400394344145,
+          "rotation": 0,
+          "target": "3-bat_e_3_"
+        },
+        {
+          "yaw": 2.571761626871613,
+          "pitch": 0.1823171261893748,
+          "rotation": 0,
+          "target": "8-bat_e_2_"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8197408519058555,
+          "pitch": 0.17112843084111873,
+          "title": "Système sécurité",
+          "text": "Des alarmes incendies sont disséminées au sein de l'établissement afin d'assurer la sécurité de façon efficace."
+        }
+      ]
+    },
+    {
+      "id": "3-bat_e_3_",
+      "name": "Bat_e_3_",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.8059390508195534,
+        "pitch": 0.2021355454408429,
+        "fov": 1.5148582672306872
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4987159298979442,
+          "pitch": 0.3721307613723823,
+          "rotation": 0,
+          "target": "6-vercors_"
+        },
+        {
+          "yaw": -2.3716476696590494,
+          "pitch": 0.30150871107114874,
+          "rotation": 0,
+          "target": "4-bat_e_4_"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1238080390599396,
+          "pitch": 0.06525399263655274,
+          "title": "Ascenseur",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">Ascenseur présent dans les bâtiments A et E afin de permettre aux élèves en incapacité de prendre les escaliers d'aller en cours dans les étages.</span>"
+        }
+      ]
+    },
+    {
+      "id": "4-bat_e_4_",
+      "name": "Bat_e_4_",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.8317621414691203,
+        "pitch": 0.13959462858466054,
+        "fov": 1.4838353065173657
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1947103880938243,
+          "pitch": 0.29689689483899606,
+          "rotation": 0,
+          "target": "5-prof"
+        },
+        {
+          "yaw": -0.83912716279292,
+          "pitch": 0.23890996628975536,
+          "rotation": 0,
+          "target": "7-bat_e_5_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-prof",
+      "name": "Prof",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.0246458867357404,
+        "pitch": 0.13845375673715488,
+        "fov": 1.525601287706261
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4062361474142442,
+          "pitch": 0.3033510783085127,
+          "rotation": 0,
+          "target": "4-bat_e_4_"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5043801052589636,
+          "pitch": 0.051375355434897685,
+          "title": "Petit électro-ménager",
+          "text": "Ces appareils électro-ménagers sont mis à disposition des professeurs durant leurs pauses dans le but de les rendre plus agréables."
+        }
+      ]
+    },
+    {
+      "id": "6-vercors_",
+      "name": "Vercors_",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.427037402737753,
+        "pitch": 0.3443798727238665,
+        "fov": 1.525601287706261
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.341409315190713,
+          "pitch": 0.2807721828212646,
+          "rotation": 0,
+          "target": "3-bat_e_3_"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.4258030598880786,
+          "pitch": 0.23572682911344245,
+          "title": "Réunion",
+          "text": "Ici se réunissent les professeurs pour les conseils de classe ainsi que les différents comités d'événements et club au sein du lycée dans le but de prendre et de discuter des décisions importantes."
+        }
+      ]
+    },
+    {
+      "id": "7-bat_e_5_",
+      "name": "Bat_e_5_",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.270367861624047,
+        "pitch": 0.22804107089830694,
+        "fov": 1.525601287706261
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6312132624603493,
+          "pitch": 0.12134168851146043,
+          "rotation": 0,
+          "target": "8-bat_e_2_"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.996600880250286,
+          "pitch": 0.3508720885672041,
+          "title": "Sortie de secours",
+          "text": "Tout comme les alarmes incendies, ces sorties de secours sont présentes un peu partout afin de permettre une mise en sécurité rapide."
+        }
+      ]
+    },
+    {
+      "id": "8-bat_e_2_",
+      "name": "Bat_e_2_",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.6695465726772127,
+        "pitch": 0.1778784790339003,
+        "fov": 1.525601287706261
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1824186272888113,
+          "pitch": 0.34514883040117894,
+          "rotation": 0,
+          "target": "9-cour-a"
+        },
+        {
+          "yaw": -2.00145858447371,
+          "pitch": 0.2272235402552134,
+          "rotation": 0,
+          "target": "2-bat_e"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.5844737678346457,
+          "pitch": -0.001110484284396307,
+          "title": "Bureaux des responsables du lycée",
+          "text": "C'est ici que les dirigeants du lycée ont leurs bureaux."
+        }
+      ]
+    },
+    {
+      "id": "9-cour-a",
+      "name": "Cour A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.989968962455526,
+        "pitch": -0.011942276985305256,
+        "fov": 1.525601287706261
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.43498119373014177,
+          "pitch": 0.09056189079578658,
+          "rotation": 0,
+          "target": "10-logements"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.7410998150187798,
+          "pitch": 0.08252875480504329,
+          "title": "Point d'appel de rentrée",
+          "text": "Ici se réunissent tous les élèves au début de l'année afin de se voir attribuer leur classe."
+        }
+      ]
+    },
+    {
+      "id": "10-logements",
+      "name": "Logements",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.1019491567718482,
+        "pitch": -0.07173941352889912,
+        "fov": 1.525601287706261
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.0163160801279822,
+          "pitch": -0.15154926699742965,
+          "title": "Résidences",
+          "text": "Des logements sont mis à disposition pour le personnel administratif du lycée afin d'éviter de potentiels déplacements journaliers."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
---
+++ visite/Theme6/Marzipano/data.js
@@ -0,0 +1,549 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entre-du-lyce",
+      "name": "Entrée du Lycée",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.5130932614232844,
+        "pitch": 0.13544133904591504,
+        "fov": 1.525601287706261
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4287666268639097,
+          "pitch": -0.014976528358126018,
+          "rotation": 0,
+          "target": "1-rue"
+        },
+        {
+          "yaw": 2.3712548234745636,
+          "pitch": -0.1258474652324253,
+          "rotation": 0,
+          "target": "8-stade_ext"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.509673492586545,
+          "pitch": 0.03995702804789403,
+          "title": "A l'aide !!!!",
+          "text": "... pour de faux"
+        },
+        {
+          "yaw": -0.29218560886384104,
+          "pitch": -0.2929520137074082,
+          "title": "Lycée Jean Prévost",
+          "text": "Notre lycée"
+        }
+      ]
+    },
+    {
+      "id": "1-rue",
+      "name": "Rue",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.066400506735981,
+        "pitch": 0.10676988133435295,
+        "fov": 1.525601287706261
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.033359957279954,
+          "pitch": 0.059615892488334765,
+          "rotation": 0,
+          "target": "0-entre-du-lyce"
+        },
+        {
+          "yaw": -2.1581171036886424,
+          "pitch": -0.01231183667759339,
+          "rotation": 0,
+          "target": "2-entre-parking-gymnase"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.1263779558335667,
+          "pitch": 0.573278426407402,
+          "title": "place pour les cars (parfois en retard)",
+          "text": "Ils peuvent être à l'heure aussi !"
+        }
+      ]
+    },
+    {
+      "id": "2-entre-parking-gymnase",
+      "name": "Entrée parking gymnase",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.8921054216940476,
+        "pitch": 0.17876492702866464,
+        "fov": 1.525601287706261
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9796718414940937,
+          "pitch": 0.10212855811583843,
+          "rotation": 0,
+          "target": "1-rue"
+        },
+        {
+          "yaw": -2.55797921211688,
+          "pitch": 0.10282015781568177,
+          "rotation": 0,
+          "target": "3-skatepark"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.487089682704518,
+          "pitch": 0.3999705822313473,
+          "title": "siège pour l'attente du car",
+          "text": "..."
+        }
+      ]
+    },
+    {
+      "id": "3-skatepark",
+      "name": "Skatepark",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.7443041009159295,
+        "pitch": 0.2022379841039399,
+        "fov": 1.525601287706261
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1788610699829647,
+          "pitch": 0.048032945687667805,
+          "rotation": 0,
+          "target": "2-entre-parking-gymnase"
+        },
+        {
+          "yaw": -2.5401958390264596,
+          "pitch": 0.11413273382920508,
+          "rotation": 0,
+          "target": "5-skatepark-ct-gymnase"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.35735280392349367,
+          "pitch": 0.035462944030514976,
+          "title": "je cherche mon trajet",
+          "text": "..."
+        },
+        {
+          "yaw": 2.0033891746142816,
+          "pitch": 0.3365455323995832,
+          "title": "rambarde glissante",
+          "text": "attention aux acrobaties"
+        }
+      ]
+    },
+    {
+      "id": "4-city",
+      "name": "City",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 200,
+      "initialViewParameters": {
+        "yaw": 0.3558135804609588,
+        "pitch": 0.10298281141402121,
+        "fov": 1.525601287706261
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4865308879435677,
+          "pitch": -0.10490660005169339,
+          "rotation": 0,
+          "target": "5-skatepark-ct-gymnase"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6673647897903336,
+          "pitch": 0.4304718542912056,
+          "title": "Attention sol glissant",
+          "text": "Oups"
+        }
+      ]
+    },
+    {
+      "id": "5-skatepark-ct-gymnase",
+      "name": "Skatepark côté gymnase",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9030387382863871,
+          "pitch": 0.1218060514235706,
+          "rotation": 0,
+          "target": "3-skatepark"
+        },
+        {
+          "yaw": 0.8013349932488776,
+          "pitch": 0.13418754852900783,
+          "rotation": 0,
+          "target": "4-city"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.717320082706956,
+          "pitch": -0.18248022290126187,
+          "title": "gymnase Jean Prévost",
+          "text": "..."
+        },
+        {
+          "yaw": 2.3573208768705713,
+          "pitch": -0.008107380785686402,
+          "title": "voiture du prof de sport",
+          "text": "..."
+        }
+      ]
+    },
+    {
+      "id": "6-pingpong",
+      "name": "Pingpong",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3306861702152943,
+          "pitch": 0.016482191469251006,
+          "rotation": 0,
+          "target": "7-dojo"
+        },
+        {
+          "yaw": 1.1928770916350597,
+          "pitch": -0.00806275122046074,
+          "rotation": 0,
+          "target": "9-stade_in"
+        },
+        {
+          "yaw": -0.38448426941993574,
+          "pitch": 0.030619036923171805,
+          "rotation": 0,
+          "target": "0-entre-du-lyce"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5435077029475828,
+          "pitch": -0.0032335388923065977,
+          "title": "photos des frères Lebrun",
+          "text": "deux champions"
+        }
+      ]
+    },
+    {
+      "id": "7-dojo",
+      "name": "Dojo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.2882445484994687,
+        "pitch": 0.271826748776391,
+        "fov": 1.525601287706261
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9418791188628113,
+          "pitch": 0.06737856999379943,
+          "rotation": 0,
+          "target": "6-pingpong"
+        },
+        {
+          "yaw": 1.828505641170656,
+          "pitch": 0.009472058436179154,
+          "rotation": 0,
+          "target": "9-stade_in"
+        },
+        {
+          "yaw": -2.8016241290142574,
+          "pitch": 0.06157936373051598,
+          "rotation": 0,
+          "target": "0-entre-du-lyce"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6951688783969843,
+          "pitch": 0.03201376683887247,
+          "title": "sac préféré du prof",
+          "text": "pour décompresser"
+        }
+      ]
+    },
+    {
+      "id": "8-stade_ext",
+      "name": "Stade_ext",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.686425224107527,
+        "pitch": 0.029500577386588844,
+        "fov": 1.525601287706261
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0916438631790832,
+          "pitch": 0.07818733705523684,
+          "rotation": 0,
+          "target": "9-stade_in"
+        },
+        {
+          "yaw": -2.954330021693835,
+          "pitch": -0.04673204056572544,
+          "rotation": 0,
+          "target": "0-entre-du-lyce"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5752252913396099,
+          "pitch": -0.258967740291725,
+          "title": "gymnase Christian Gand",
+          "text": "un autre gymnase..."
+        },
+        {
+          "yaw": 0.33934515100666474,
+          "pitch": -0.09373286725111996,
+          "title": "Piscine Belle Etoile",
+          "text": "..."
+        }
+      ]
+    },
+    {
+      "id": "9-stade_in",
+      "name": "Stade_in",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.046293794183998926,
+        "pitch": 0.02949378755410237,
+        "fov": 1.525601287706261
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4551477919870592,
+          "pitch": 0.0498082150088095,
+          "rotation": 0,
+          "target": "7-dojo"
+        },
+        {
+          "yaw": -0.5605959617683389,
+          "pitch": 0.07029004114643556,
+          "rotation": 0,
+          "target": "6-pingpong"
+        },
+        {
+          "yaw": 1.9090161794964091,
+          "pitch": -0.06331071947437295,
+          "rotation": 0,
+          "target": "8-stade_ext"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.288357360281891,
+          "pitch": -0.1486711822407596,
+          "title": "image bugguée",
+          "text": "..."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
